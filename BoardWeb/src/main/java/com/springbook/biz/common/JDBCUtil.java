13410acabfada3730ab4a009c8c8b57bfca12b46
Leejiho-940915 <wlgh4585@daum.net>
2022-09-06 23:07:05 +0900

    board-add_2022-09-06

---
+++ BoardWeb/src/main/java/com/springbook/biz/common/JDBCUtil.java
@@ -0,0 +1,90 @@
+package com.springbook.biz.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class JDBCUtil {
+	
+	public static Connection getConnection() {
+		try {
+			
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			return DriverManager.getConnection("jdbc:oracle:thin:@192.168.111.114:1521:ORCL", "board", "board");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}// end of getConnection
+	
+	public static void close(PreparedStatement stmt, Connection conn) {
+		if (stmt != null) {
+			
+			try {
+				
+				if (!stmt.isClosed()) stmt.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				stmt = null;
+			}
+		}// end of stmt
+		
+		if (conn != null) {
+			
+			try {
+				
+				if (!conn.isClosed()) conn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn = null;
+			}
+		}// end of conn
+	}// end of close
+
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		if (rs != null) {
+			
+			try {
+				
+				if (!rs.isClosed()) rs.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				rs = null;
+			}
+		}// end of rs
+		
+		if (stmt != null) {
+			
+			try {
+				
+				if (!stmt.isClosed()) stmt.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				stmt = null;
+			}
+		}// end of stmt
+		
+		if (conn != null) {
+			
+			try {
+				
+				if (!conn.isClosed()) conn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn = null;
+			}
+		}// end of conn
+	}// end of close
+}
